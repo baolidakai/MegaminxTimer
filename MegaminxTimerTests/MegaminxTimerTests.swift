6965bacdb320928f706242beb9b535763517514f
bowendeng <bowendeng@bowendeng-macbookpro.roam.corp.google.com>
2016-05-10 21:50:30 -0700

    first draft

---
+++ MegaminxTimerTests/MegaminxTimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  MegaminxTimerTests.swift
+//  MegaminxTimerTests
+//
+//  Created by bowendeng on 5/9/16.
+//  Copyright © 2016 bowendeng. All rights reserved.
+//
+
+import XCTest
+@testable import MegaminxTimer
+
+class MegaminxTimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
